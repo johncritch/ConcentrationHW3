b04e72f04db7a4aff92a749a3714157b76c768c6
johncritch <johnnyrcritch@gmail.com>
2022-09-07 00:32:53 -0600

    Initial Commit

---
+++ ConcentrationHW3/ConcentrationHW3App.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationHW3App.swift
+//  ConcentrationHW3
+//
+//  Created by John Critchlow on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationHW3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
